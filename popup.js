14d8b8a33b252cee4a444dc758dcd79d1b72beee
TiborStachoske <tibor.stachoske@web-for-life.com>
2021-10-31 22:47:58 +0100

    remove: Twitch streams from broadcast schedule

--- popup.js
+++ popup.js
@@ -8,37 +8,38 @@ async function loadData() {
     let data = await sendeplanJson.json();
 
     data.data.forEach(days => {
+        console.log(days);
         let sendeDate = (days.date).split('T');
 
         if (sendeDate[0] == dayNow) {
             days.elements.forEach(sendung => {
-                let sendeTime = new Date(sendung.timeStart);
-                let onAir, live, showImage;
+                if(sendung.channelGroups[0].type != "talent"){
+                    let sendeTime = new Date(sendung.timeStart);
+                    let onAir, live, showImage;
 
-                let type = (sendung.type != "rerun") ? '<div class="dot ' + sendung.type + '"></div>' : '';
+                    let type = (sendung.type != "rerun") ? '<div class="dot ' + sendung.type + '"></div>' : '';
 
-                let vod = (sendung.type != "rerun") ? '' : '<a href="https://rocketbeans.tv/mediathek/video/' + sendung.episodeId + '" target="_blank"><div class="vod"><img src="images/svg/yt.svg"></div></a></div>';
+                    let vod = (sendung.type != "rerun") ? '' : '<a href="https://rocketbeans.tv/mediathek/video/' + sendung.episodeId + '" target="_blank"><div class="vod"><img src="images/svg/yt.svg"></div></a></div>';
 
-                if ((new Date()).getTime() > new Date(sendung.timeStart).getTime() && (new Date()).getTime() < new Date(sendung.timeEnd).getTime()) {
-                    onAir = '<div class="onair"></div>';
-                    live = 'live'
-                } else {
-                    onAir = '';
-                    live = '';
+                    if ((new Date()).getTime() > new Date(sendung.timeStart).getTime() && (new Date()).getTime() < new Date(sendung.timeEnd).getTime()) {
+                        onAir = '<div class="onair"></div>';
+                        live = 'live'
+                    } else {
+                        onAir = '';
+                        live = '';
+                    }
+
+                    let time = sendung.duration / 60;
+                    let minutes = time % 60;
+                    let hours = Math.floor(time / 60);
+                    let timeOutput = ((hours > 0) ? hours + ' Std ' + ((minutes > 0) ? minutes + ' Min' : '') : minutes + ' Min')
+
+                    showImage = (sendung.episodeImage != null) ? '<div class="show-image" style="background-image: url(' + sendung.episodeImage + ');"></div>' : '<div class="show-image" style="background-image: url(images/placeholder.png);"></div>'
+                    content.innerHTML += '<div class="box" id="' + live + '">' + type + '<div class="time">' + (sendeTime.getHours() + ':' + ("0" + sendeTime.getMinutes()).slice(-2)).toString() + ' Uhr</div>' +
+                        '<span class="title">' + sendung.title + '</span><br>' + sendung.topic +
+                        '<div class="dauer">' + timeOutput + '</div>' + onAir + showImage + '<div>' +
+                        vod;
                 }
-
-                let time = sendung.duration / 60;
-                let minutes = time % 60;
-                let hours = Math.floor(time / 60);
-
-                let timeOutput = ((hours > 0) ? hours + ' Std ' + ((minutes > 0) ? minutes + ' Min' : '') : minutes + ' Min')
-
-                showImage = (sendung.episodeImage != null) ? '<div class="show-image" style="background-image: url(' + sendung.episodeImage + ');"></div>' : '<div class="show-image" style="background-image: url(images/placeholder.png);"></div>'
-
-                content.innerHTML += '<div class="box" id="' + live + '">' + type + '<div class="time">' + (sendeTime.getHours() + ':' + ("0" + sendeTime.getMinutes()).slice(-2)).toString() + ' Uhr</div>' +
-                    '<span class="title">' + sendung.title + '</span><br>' + sendung.topic +
-                    '<div class="dauer">' + timeOutput + '</div>' + onAir + showImage + '<div>' +
-                    vod;
             });
 
             const element = document.getElementById('live');
