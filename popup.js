00f29a4df3c6b6afacefe217d1bff91800428c2e
TiborStachoske <tibor.stachoske@web-for-life.com>
2021-11-01 14:04:03 +0100

    bugfix

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 let content = document.getElementById("content");
 let date = new Date();
-let yesterday = new Date((date.setDate(date.getDate() - 1)));
-let dayNow = date.getFullYear() + '-' + ("0" + (yesterday.getMonth() + 1)).slice(-2) + '-' + ("0" + yesterday.getDate()).slice(-2);
+// let yesterday = new Date((date.setDate(date.getDate() - 1)));
+let dayNow = date.getFullYear() + '-' + ("0" + (date.getMonth() + 1)).slice(-2) + '-' + ("0" + date.getDay()).slice(-2);
 
 async function loadData() {
     let sendeplanJson = await fetch('https://api.rocketbeans.tv/v1/schedule/normalized/?startDay=' + Math.round(new Date().getTime() / 1000));
@@ -10,36 +10,36 @@ async function loadData() {
     data.data.forEach(days => {
         console.log(days);
         let sendeDate = (days.date).split('T');
+        console.log(sendeDate[0]);
+        console.log(dayNow);
 
         if (sendeDate[0] == dayNow) {
             days.elements.forEach(sendung => {
-                if(sendung.channelGroups[0].type != "talent"){
-                    let sendeTime = new Date(sendung.timeStart);
-                    let onAir, live, showImage;
+                let sendeTime = new Date(sendung.timeStart);
+                let onAir, live, showImage;
 
-                    let type = (sendung.type != "rerun") ? '<div class="dot ' + sendung.type + '"></div>' : '';
+                let type = (sendung.type != "rerun") ? '<div class="dot ' + sendung.type + '"></div>' : '';
 
-                    let vod = (sendung.type != "rerun") ? '' : '<a href="https://rocketbeans.tv/mediathek/video/' + sendung.episodeId + '" target="_blank"><div class="vod"><img src="images/svg/yt.svg"></div></a></div>';
+                let vod = (sendung.type != "rerun") ? '' : '<a href="https://rocketbeans.tv/mediathek/video/' + sendung.episodeId + '" target="_blank"><div class="vod"><img src="images/svg/yt.svg"></div></a></div>';
 
-                    if ((new Date()).getTime() > new Date(sendung.timeStart).getTime() && (new Date()).getTime() < new Date(sendung.timeEnd).getTime()) {
-                        onAir = '<div class="onair"></div>';
-                        live = 'live'
-                    } else {
-                        onAir = '';
-                        live = '';
-                    }
-
-                    let time = sendung.duration / 60;
-                    let minutes = time % 60;
-                    let hours = Math.floor(time / 60);
-                    let timeOutput = ((hours > 0) ? hours + ' Std ' + ((minutes > 0) ? minutes + ' Min' : '') : minutes + ' Min')
-
-                    showImage = (sendung.episodeImage != null) ? '<div class="show-image" style="background-image: url(' + sendung.episodeImage + ');"></div>' : '<div class="show-image" style="background-image: url(images/placeholder.png);"></div>'
-                    content.innerHTML += '<div class="box" id="' + live + '">' + type + '<div class="time">' + (sendeTime.getHours() + ':' + ("0" + sendeTime.getMinutes()).slice(-2)).toString() + ' Uhr</div>' +
-                        '<span class="title">' + sendung.title + '</span><br>' + sendung.topic +
-                        '<div class="dauer">' + timeOutput + '</div>' + onAir + showImage + '<div>' +
-                        vod;
+                if (sendung.channelGroups.type != "talent" && (new Date()).getTime() > new Date(sendung.timeStart).getTime() && (new Date()).getTime() < new Date(sendung.timeEnd).getTime()) {
+                    onAir = '<div class="onair"></div>';
+                    live = 'live'
+                } else {
+                    onAir = '';
+                    live = '';
                 }
+
+                let time = sendung.duration / 60;
+                let minutes = time % 60;
+                let hours = Math.floor(time / 60);
+                let timeOutput = ((hours > 0) ? hours + ' Std ' + ((minutes > 0) ? minutes + ' Min' : '') : minutes + ' Min')
+
+                showImage = (sendung.episodeImage != null) ? '<div class="show-image" style="background-image: url(' + sendung.episodeImage + ');"></div>' : '<div class="show-image" style="background-image: url(images/placeholder.png);"></div>'
+                content.innerHTML += '<div class="box" id="' + live + '">' + type + '<div class="time">' + (sendeTime.getHours() + ':' + ("0" + sendeTime.getMinutes()).slice(-2)).toString() + ' Uhr</div>' +
+                    '<span class="title">' + sendung.title + '</span><br>' + sendung.topic +
+                    '<div class="dauer">' + timeOutput + '</div>' + onAir + showImage + '<div>' +
+                    vod;
             });
 
             const element = document.getElementById('live');
